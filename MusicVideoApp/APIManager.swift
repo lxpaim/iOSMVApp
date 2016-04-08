c99d2ef57ab2c78b46cd00987059b87a8a88ef60
Célio Lisboa <celiolisboa@macbook-pro-de-celio.local>
2016-04-08 09:44:46 +0100

    Added json serialization

--- MusicVideoApp/APIManager.swift
+++ MusicVideoApp/APIManager.swift
@@ -21,15 +21,37 @@ class APIManager {
             
             (data,response,error) ->Void in
             
-            dispatch_async(dispatch_get_main_queue()){
+            
                 if error != nil {
+                    dispatch_async(dispatch_get_main_queue()) {
                     completion(result: error!.localizedDescription)
+                    }
                 }
                 else{
-                    completion(result: "NSURLSession sucessful ")
                     print(data)
+                    do{
+                        
+                        if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+                            as? [String:AnyObject] {
+                            print (json)
+                            
+                            
+                            let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                                
+                                dispatch_async(dispatch_get_global_queue( priority, 0)) {
+                                dispatch_async(dispatch_get_main_queue()) {
+                                    completion(result: "JSONSerialization sucessful")
+                                }
+                            }
+                        }
+                        
+                    }catch{
+                        dispatch_async(dispatch_get_main_queue()) {
+                            completion(result: "Error in NSJSONSerialization")
+                        }
+                    }
                 }
-            }
+            
         }
         task.resume()
 
