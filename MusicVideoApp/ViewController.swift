c11f825c2f6185f50f27fe8d03e7248af23a44ed
Célio Lisboa <celiolisboa@macbook-pro-de-celio.local>
2016-04-09 22:29:51 +0100

    Added logic to change main view controller color if reachability changes Adem logic for view controller to register and change if reachability changes

--- MusicVideoApp/ViewController.swift
+++ MusicVideoApp/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.reachabilityStatusChanged), name: kReachabilityChangedNotification , object: nil)
+        
+        
+        reachabilityStatusChanged()
+        
+        
         //Call API
         let url = "https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json"
         let apiManager = APIManager()
@@ -31,6 +39,30 @@ class ViewController: UIViewController {
             print("Videos in Top Ten \(video.vName)")
         }
     }
+    
+    func reachabilityStatusChanged(){
+        
+        switch reachabilityStatus {
+            
+        case NOACESS:
+            view.backgroundColor = UIColor.redColor()
+            displayLabel.text = "No internet"
+        case WIFI:
+            view.backgroundColor = UIColor.greenColor()
+            displayLabel.text = "Reachable with Wi-Fi"
+        case WWAN:
+            view.backgroundColor = UIColor.yellowColor()
+            displayLabel.text = "Reachabel with Mobile Data"
+        default:return
+            
+        }
+    }
+    
+    // Remove the observer from the view Controller
+    deinit{
+     NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachabilityStatusChanged", object: nil)
+    }
+    
 
 }
 
