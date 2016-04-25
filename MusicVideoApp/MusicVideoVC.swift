1a009326422cffe68ae0f761e21a96cb687673ba
Célio Lisboa <celiolisboa@macbook-pro-de-celio.local>
2016-04-25 05:27:13 +0100

    Added authentication for share button

--- MusicVideoApp/MusicVideoVC.swift
+++ MusicVideoApp/MusicVideoVC.swift
@@ -9,10 +9,13 @@
 import UIKit
 import AVKit
 import AVFoundation
+import LocalAuthentication
 
 class MusicVideoVC: UIViewController {
 
     var videos: Videos!
+    
+    var securitySwitch:Bool = false
     
     @IBOutlet weak var musicVideoName: UILabel!
     
@@ -28,9 +31,90 @@ class MusicVideoVC: UIViewController {
     
     
     @IBAction func socialMedia(sender: UIBarButtonItem) {
+        securitySwitch = NSUserDefaults.standardUserDefaults().boolForKey("SecSetting")
         
-        shareMedia()
+        switch securitySwitch {
+        case true:
+            touchIdCheck()
+        default:
+             shareMedia()
+        }
         
+       
+        
+    }
+    func touchIdCheck()->Bool{
+        // Create an alert 
+        let alert = UIAlertController(title: "", message: "", preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "Continue", style: .Cancel, handler: nil))
+            
+        //Create local auth context 
+        let context = LAContext()
+        var touchIDError: NSError?
+        let reasonString = "Touch is required to share data on network"
+        
+        // check if local auth is avaliable
+        if context.canEvaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, error: &touchIDError) {
+            // check device response
+            context.evaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, localizedReason: reasonString, reply: { (sucess, policyError) -> Void in
+                if sucess {
+                    dispatch_async(dispatch_get_main_queue(), {[unowned self] in self.shareMedia()})
+                }else{
+                    alert.title = "Unsucessful"
+                    
+                    switch LAError(rawValue: policyError!.code)!{
+                    case .AppCancel:
+                        alert.message = "Authtentication was cancelled by application"
+                    case .AuthenticationFailed:
+                        alert.message = "User failed to provide valid credentials"
+                    case .PasscodeNotSet:
+                        alert.message = "Passcode is not set on device"
+                    case .SystemCancel:
+                        alert.message = "Application was cancelled by the system"
+                    case .TouchIDLockout:
+                        alert.message = "Too many failed atempts"
+                    case .TouchIDNotAvailable:
+                        alert.message = "Device does not support touch id"
+                    case .UserCancel:
+                        alert.message = "User cancelled"
+                    case .UserFallback:
+                        alert.message = "Passcode not accepted must use touch ID"
+                    default:
+                        alert.message = "Unable to authenticate"
+                    }
+                    dispatch_async(dispatch_get_main_queue(), {
+                        [unowned self] in self.presentViewController(alert, animated: true, completion: nil)
+                        })
+
+                }
+            })
+        }else {
+            
+            alert.title = "Error"
+            
+            switch LAError(rawValue: touchIDError!.code)!{
+    
+            case .TouchIDNotEnrolled:
+                alert.message = "TouchID not enrolled"
+            case .TouchIDNotAvailable:
+                alert.message = "Device does not support touch id"
+            case .UserCancel:
+                alert.message = "User cancelled"
+            case .UserFallback:
+                alert.message = "Passcode not accepted must use touch ID"
+            case .InvalidContext:
+                alert.message = "Context is invalid"
+            case .PasscodeNotSet:
+                alert.message = "Passcode not set"
+            default:
+                alert.message = "Local authentication is not avaliable"
+            }
+            dispatch_async(dispatch_get_main_queue(), {
+                [unowned self] in self.presentViewController(alert, animated: true, completion: nil)
+                })
+
+        }
+        return false
     }
     
     func shareMedia(){
@@ -81,6 +165,7 @@ class MusicVideoVC: UIViewController {
             musicVideoImage.image = UIImage(named: "no_image")
         }
 
+       
         // Do any additional setup after loading the view.
     }
     
